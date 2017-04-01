448b4a06a33c65ce183e7e1bc27a5633b835350c
Yarikua <mr.yarikua@gmail.com>
2017-04-01 09:39:52 +0200

    2-variabels

---
+++ JS/2-variables.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var imie = "Jarek";
+var imieDamskie ="Kaja";
+var wiek = 37;
+
+var kolor ="cerwony";
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
+
+wiek = 21;
+
+console.log(wiek);
