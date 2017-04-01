c944b52c2a92f6e8e3f85d3a43c1a2202a45e6c7
Yarikua <mr.yarikua@gmail.com>
2017-04-01 10:43:51 +0200

    Typy Dannych

---
+++ JS/3-typy-dannych.js
@@ -0,0 +1,43 @@
+var wyplata = 5000;
+var premia = 3000;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+
+
+/*Typ - lancuch znakow - string*/
+
+var wyplataStr = "5000";
+var premiaStr = 3000;
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr+premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+
+/*Typ logiczny - Boolean*/
+
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog){
+    console.log("Jest SMOG");
+} else{
+    console.log("Nie ma Smoga");
+}
+
+/*Typ Sprecjalne - undefined / null*/
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+var imieStudenta = "Michał";
+console.log(imieStudenta.indexOf('c'));
+
