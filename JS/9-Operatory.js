3d0eaa8594d776aff6863a69b5d02ca05b35e286
Yarikua <mr.yarikua@gmail.com>
2017-04-01 14:20:28 +0200

    Operatory

---
+++ JS/9-Operatory.js
@@ -0,0 +1,27 @@
+/*
+'use strict';
+
+var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+
+while (liczba2 < liczba1){
+    console.log(liczba1);
+    --liczba1;
+}
+*/
+
+
+if ("2" === 2){
+    console.log("prawda");
+} else {
+    console.log("Nie prawda");
+}
+
+if ( !(("2" !==2) && (8 < 4))){
+    document.write("<br> prawda")
+} 
